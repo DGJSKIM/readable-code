001143b2c3e9459c0756bffeadc5256d924f486b
DGJSKIM <kjsaj0525@gmail.com>
2025-06-08 22:38:16 +0900

    29. DIP

--- src/main/java/cleancode/minesweeper/tobe/GameApplication.java
+++ src/main/java/cleancode/minesweeper/tobe/GameApplication.java
@@ -2,14 +2,33 @@ package cleancode.minesweeper.tobe;
 
 import cleancode.minesweeper.tobe.gamelevel.GameLevel;
 import cleancode.minesweeper.tobe.gamelevel.VeryBeginner;
+import cleancode.minesweeper.tobe.io.ConsoleInputHandler;
+import cleancode.minesweeper.tobe.io.ConsoleOutputHandler;
+import cleancode.minesweeper.tobe.io.InputHandler;
+import cleancode.minesweeper.tobe.io.OutputHandler;
 
 public class GameApplication {
 
     public static void main(String[] args) {
         GameLevel gameLevel = new VeryBeginner();
 
-        Minesweeper minesweeper = new Minesweeper(gameLevel);
+        InputHandler inputHandler = new ConsoleInputHandler();
+        OutputHandler outputHandler = new ConsoleOutputHandler();
+
+        Minesweeper minesweeper = new Minesweeper(gameLevel, inputHandler, outputHandler);
         minesweeper.initialize();
         minesweeper.run();
     }
+
+    /**
+     * DIP (Dependency Inversion principle)
+     *
+     * DI (Dependency Injection) - "3"
+     * 필요한 의존성을 외부에서 주입받는다
+     * 제 3자가 항상 두 객체 간 의존성을 맺어주고 주입해준다
+     *
+     * IoC (Inversion of Control)
+     *
+     * -> 얘네는 따로 정리해봐도 좋을 듯
+     * */
 }
